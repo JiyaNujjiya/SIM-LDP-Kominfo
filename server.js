1468944ed69b71387664a87e2a3130143584953a
JiyaNujjiya <fida.nujjiya@mhs.itenas.ac.id>
2026-09-08 14:56:34 +0700

    feat: complete backend manajemen relasi pengguna

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bcpRoutes = require('./routes/bcpRoutes');
 const perubahanRoutes = require('./routes/perubahanRoutes'); 
 const tiketingRoutes = require('./routes/tiketingRoutes');
 const konteksRoutes = require('./routes/konteksRoutes');
+const relasiPenggunaRoutes = require('./routes/relasiPenggunaRoutes');
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use('/api/risiko', risikoRoutes);
 app.use('/api/bcp', bcpRoutes);   
 app.use('/api/perubahan', perubahanRoutes); 
 app.use('/api/tiketing', tiketingRoutes); 
+app.use('/api/relasi-pengguna', relasiPenggunaRoutes);
 
 
 
